343ae9cf86c4694eedc0d0ab61c93733951a6f83
Suyash <suyash.srivastava@locai.ai>
2024-09-30 20:13:36 +0400

    adding streamlit runner file

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import requests
+
+st.title("FastAPI and Streamlit Integration")
+
+# Send a request to the FastAPI backend
+api_url = "https://ocr-poc.mofkrah.ai"
+response = requests.get(api_url)
+
+if response.status_code == 200:
+    data = response.json()
+    st.write(f"Response from FastAPI: {data['message']}")
+else:
+    st.write(f"Error: Unable to fetch data from FastAPI. Status code: {response.status_code}")
+

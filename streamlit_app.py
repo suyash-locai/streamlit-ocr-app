96fd32eeaa24dbbdff23813a4d2ca9e2c3275dc5
Suyash <suyash.srivastava@locai.ai>
2024-09-30 20:18:05 +0400

    adding streamlit runner file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import requests
 
-st.title("FastAPI and Streamlit Integration")
+st.title("File Upload to FastAPI")
 
-# Send a request to the FastAPI backend
-api_url = "https://ocr-poc.mofkrah.ai"
-response = requests.get(api_url)
+# File uploader UI
+uploaded_file = st.file_uploader("Choose a file")
 
-if response.status_code == 200:
-    data = response.json()
-    st.write(f"Response from FastAPI: {data}")
-else:
-    st.write(f"Error: Unable to fetch data from FastAPI. Status code: {response.status_code}")
+if uploaded_file is not None:
+    # Convert the uploaded file to bytes
+    files = {'file': (uploaded_file.name, uploaded_file.getvalue())}
+
+    # Make a POST request to FastAPI with the uploaded file
+    api_url = "https://ocr-poc.mofkrah.ai/arabic-ocr/"
+    response = requests.post(api_url, files=files)
+
+    if response.status_code == 200:
+        st.success(f"File '{uploaded_file.name}' uploaded successfully!")
+        st.json(response.json())
+    else:
+        st.error(f"Error: Unable to upload file. Status code: {response.status_code}")
+
 
